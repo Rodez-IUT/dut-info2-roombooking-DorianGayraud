7e829566e7a4343143aed0b4de2ff2883c61b7ab
DorianGayraud <dorian.gayraud@iut-rodez.fr>
2020-03-04 09:29:15 +0100

    fix #1.2 Parser SAX

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/samples/RoomBookingBasicDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/samples/RoomBookingBasicDomParser.java
@@ -1,21 +1,27 @@
 package xmlws.roombooking.xmltools.samples;
 
 import org.w3c.dom.Document;
+import xmlws.roombooking.xmltools.RoomBooking;
+import xmlws.roombooking.xmltools.RoomBookingParser;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 /**
  *  Class for objects responsible of RoomBooking xml files parsing
  *  DOM version
  */
-public class RoomBookingBasicDomParser {
+public class RoomBookingBasicDomParser implements RoomBookingParser {
+
+    RoomBooking roomBooking;
 
     /**
      * Parse an xml file provided as an input stream
      * @param inputStream the input stream corresponding to the xml file
      */
-    public void parse(InputStream inputStream) {
+    public RoomBooking parse(InputStream inputStream) {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -25,6 +31,7 @@ public class RoomBookingBasicDomParser {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return roomBooking;
     }
 
 }
--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/samples/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/samples/RoomBookingDomParser.java
@@ -1,12 +1,13 @@
 package xmlws.roombooking.xmltools.samples;
 
-import org.w3c.dom.Document;
-import xmlws.roombooking.xmltools.RoomBooking;
-import xmlws.roombooking.xmltools.RoomBookingParser;
+        import org.w3c.dom.Document;
+        import xmlws.roombooking.xmltools.RoomBooking;
+        import xmlws.roombooking.xmltools.RoomBookingParser;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.InputStream;
+        import javax.xml.parsers.DocumentBuilder;
+        import javax.xml.parsers.DocumentBuilderFactory;
+        import java.io.InputStream;
+        import java.text.SimpleDateFormat;
 
 /**
  *  Class for objects responsible of RoomBooking xml files parsing
@@ -14,22 +15,27 @@ import java.io.InputStream;
  */
 public class RoomBookingDomParser implements RoomBookingParser {
 
-    RoomBooking roomBooking;
-
     /**
      * Parse an xml file provided as an input stream
      * @param inputStream the input stream corresponding to the xml file
      */
     public RoomBooking parse(InputStream inputStream) {
+        RoomBooking roomBooking = new RoomBooking();
+        /* Format de date */
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(inputStream);
-            System.out.println(doc.getElementsByTagName("label").item(0).getTextContent());
+            roomBooking.setRoomLabel(doc.getElementsByTagName("label").item(0).getTextContent());
+            roomBooking.setUsername(doc.getElementsByTagName("username").item(0).getTextContent());
+            roomBooking.setStartDate(sdf.parse(doc.getElementsByTagName("startDate").item(0).getTextContent()));
+            roomBooking.setEndDate(sdf.parse(doc.getElementsByTagName("endDate").item(0).getTextContent()));
             System.out.println(doc.getElementsByTagName("username").item(0).getTextContent());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         return roomBooking;
     }
 
